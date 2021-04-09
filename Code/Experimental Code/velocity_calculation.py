4c7a5fa0ce153d7afadf04173d0ff7849a05f825
Høgni <hogniziska@gmail.com>
2021-04-09 12:09:30 +0200

    Sorry :(

--- Code/Experimental Code/velocity_calculation.py
+++ Code/Experimental Code/velocity_calculation.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import datetime
 import numpy as np
@@ -6,6 +5,7 @@ import math
 
 # Velocity Calculations
 # ------------------------------------------
+
 
 def calculate_speed(distance, df):
     speed_dictionary = {}
@@ -17,22 +17,24 @@ def calculate_speed(distance, df):
 
     for key, value in speed_dictionary.copy().items():
         if value[0] > value[1]:
-            difference = (value[0]-value[1])
+            difference = value[0] - value[1]
         else:
-            difference = (value[1]-value[0])
+            difference = value[1] - value[0]
         speed_dictionary[key] = difference
 
     for key, value in speed_dictionary.copy().items():
         t = speed_dictionary[key].total_seconds()
         if t == 0:
             t = 1
-            speed_dictionary[key] = distance/t
+            speed_dictionary[key] = distance / t
         else:
-            speed_dictionary[key] = distance/t
+            speed_dictionary[key] = distance / t
     return speed_dictionary
 
-# Absolute Heading 
+
+# Absolute Heading
 # ------------------------------------------
+
 
 def calculate_true_heading(frame_orientation, bearing):
     true_heading = bearing + frame_orientation
