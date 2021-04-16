a49b7355e2c5d4edeea817cbf4d54b46f0766ef2
Høgni <hogniziska@gmail.com>
2021-04-17 01:37:56 +0200

    Added ID method, requirements and main changes

--- Code/connect.py
+++ Code/connect.py
@@ -1,280 +1,33 @@
-from math import atan2, cos, sin, degrees
+#!git clone https://github.com/abewley/sort.git
 import pandas as pd
-import math
-import morph
-# from shapely.geometry import Point
-# from shapely.geometry.polygon import Polygon
 import numpy as np
-import matplotlib.pyplot as plt
-# from frechetdist import frdist
-import similaritymeasures
-import sys
-from multiprocessing import Process
-# import threading
-pd.options.mode.chained_assignment = None
+from sort import *
 
-def itterate_through_df_in_chunks_and_apply(tracker_df):
-    tracker_df_grouped = tracker_df.groupby("UniqueID")
-    for name, group in tracker_df_grouped:
-        length = len(group)
-        last_bearing = dict()
-        last_distance = dict()
+def set_unique_id(df, max_age=30, min_hits=1, iou_threshold=0.15):
+    tracker = Sort(max_age, min_hits, iou_threshold)
 
-        for i in range(length):
-            previous_i = i + 2
-            cut_group = group[i:previous_i].sort_values(by=["frameId"])
-            len_small_group = len(cut_group)
-            if len_small_group == 1:
-                try:
-                    tracker_df.at[cut_group.index[0], "new_bearing"] = last_bearing[cut_group.iloc[0]["UniqueID"]]
-                    # tracker_df.at[cut_group.index[0], "Distance"] = last_distance[cut_group.iloc[0]["UniqueID"]]
-                except KeyError:
-                    tracker_df.at[cut_group.index[0], "new_bearing"] = 0
-                    # tracker_df.at[cut_group.index[0], "Distance"] = 0
-            else:
-                bearing = calculate_bearing(cut_group)
-                # distance = pixel_distance(cut_group)
-                tracker_df.at[cut_group.index[0], "new_bearing"] = bearing
-                # tracker_df.at[cut_group.index[0], "Distance"] = distance
-                last_bearing[cut_group.iloc[0]["UniqueID"]] = bearing
-                # last_distance[cut_group.iloc[0]["UniqueID"]] = distance
-                
-                if length == 2:
-                    tracker_df.at[cut_group.index[1], "new_bearing"] = bearing
-                    # tracker_df.at[cut_group.index[1], "Distance"] = distance
-    return tracker_df
+    df_grouped = df.groupby("frameId")
+    list_with_id = []
 
-def calculate_bearing(cut_group):
-    x_1, y_1 = 960, 540
-    # x_1, y_1 = cut_group.iloc[0]["smooth_x"], cut_group.iloc[0]["smooth_y"]
-    x_2, y_2 = cut_group.iloc[1]["smooth_x"], cut_group.iloc[1]["smooth_y"]
-    # angle = atan2(cos(y_1)*sin(y_2)-sin(y_1) * cos(y_2)*cos(x_2-x_1), sin(x_2-x_1)*cos(y_2))
-    # bearing = (degrees(angle) + 360) % 360
-    angle = math.atan2(y_1-y_2, x_1-x_2)
-    bearing = math.degrees(angle)
-    bearing = (bearing + 360) % 360
-    return bearing
+    for _, group in df_grouped:
+        object_list = []
+        for _, row in group.iterrows():
+            object_list.append([row["xmin"], row["ymin"], row["xmax"], row["ymax"], row["confidence"]])
+        track_with_id = tracker.update(np.array(object_list))
+        list_with_id.append(track_with_id)
 
-def add_colour(tracker_df):
-    for _, row in tracker_df.iterrows():
-        # bearing = calculate_bearing((row["altered_x"], row["altered_y"]))
-        colour = get_color(row["new_bearing_smooth"])
-        # tracker_df.at[tracker_df.index[_], "new_bearing"] = bearing
-        tracker_df.at[tracker_df.index[_], "colour_1"] = colour[0]
-        tracker_df.at[tracker_df.index[_], "colour_2"] = colour[1]
-        tracker_df.at[tracker_df.index[_], "colour_3"] = colour[2]
-        tracker_df.at[tracker_df.index[_], "colour_4"] = colour[3]
-    return tracker_df
+    df_data = []
+    for idx, i in enumerate(list_with_id):
+        for row in i:
+            x = ((row[2] - row[0])/2) + row[0]
+            y = ((row[3] - row[1])/2) + row[1]
+            h = (row[3] - row[1])
+            w = (row[2] - row[0])
+            temp = [x, y, int(row[4]), idx, "blue", h, w]
+            df_data.append(temp)
 
-def get_color(n):
-    return plt.cm.gist_ncar(int(round(n)))
-
-def smooth_bearings(tracker_df):
-    tracker_df_ = (
-        tracker_df.groupby("UniqueID")["new_bearing"]
-        .rolling(20, min_periods=1)
-        .mean()
-        .to_frame(name="new_bearing_smooth")
-        .droplevel("UniqueID")
-    )
-    tracker_df = tracker_df.join(tracker_df_)
-    return tracker_df
-
-def run_connect(tracker_df):
-    tracker_df = itterate_through_df_in_chunks_and_apply(tracker_df)
-    tracker_df = smooth_bearings(tracker_df)
-    tracker_df = add_colour(tracker_df)
-    return tracker_df
-
-# def calculate_bearing(point):
-#     x_1, y_1 = 960, 0
-#     x_2, y_2 = point[0], point[1]
-#     angle = math.atan2(y_1-y_2, x_1-x_2)
-#     bearing = math.degrees(angle)
-#     bearing = (bearing + 360) % 360
-#     return bearing
-
-
-
-# def calculate_bearing(tracker_df):
-#     tracker_df_grouped = tracker_df.groupby("UniqueID")
-#     for name, group in tracker_df_grouped:
-#         length = len(group)
-#         last_bearing = dict()
-
-#         for i in range(length):
-#             previous_i = i + 2
-#             cut_group = group[i:previous_i]
-#             len_small_group = len(cut_group)
-#             if len_small_group == 1:
-#                 try:
-#                     tracker_df.at[cut_group.index[0], "new_bearing"] = last_bearing[cut_group.iloc[0]["UniqueID"]]
-#                 except KeyError:
-#                     tracker_df.at[cut_group.index[0], "new_bearing"] = 0
-#             else:
-#                 x_1, y_1 = cut_group.iloc[0]["altered_x"], cut_group.iloc[0]["altered_y"]
-#                 x_2, y_2 = cut_group.iloc[1]["altered_x"], cut_group.iloc[1]["altered_y"]
-#                 angle = atan2(cos(y_1)*sin(y_2)-sin(y_1) * cos(y_2)*cos(x_2-x_1), sin(x_2-x_1)*cos(y_2))
-#                 bearing = (degrees(angle) + 360) % 360
-#                 tracker_df.at[cut_group.index[0], "new_bearing"] = bearing
-#                 last_bearing[cut_group.iloc[0]["UniqueID"]] = bearing
-#                 if length == 2:
-#                     tracker_df.at[cut_group.index[1], "new_bearing"] = bearing
-#     return tracker_df
-
-def pixel_distance(cut_group):
-    x_1, y_1 = cut_group.iloc[0]["altered_x"], cut_group.iloc[0]["altered_y"]
-    x_2, y_2 = cut_group.iloc[1]["altered_x"], cut_group.iloc[1]["altered_y"]
-    distance = math.sqrt((x_2 - x_1)**2 + (y_2 - y_1)**2)
-    return distance
-
-def relative_velocity(tracker_df):
-    pass
-
-# def remove_far_field_points(tracker_df, polygon_points):
-#     polygon = Polygon(polygon_points)
-#     tracker_df["Inside_polygon"] = False
-#     for _, row in tracker_df.itterows():
-#         point = Point(row["altered_x"], row["altered_y"])
-#         if polygon.contains(point):
-#             tracker_df.at[_, "Inside_polygon"] = True
-#     return tracker_df
-
-def calculate_assign(i, new_index, grouped_df_list, grouped_id_list, num = 0):
-    sim = similaritymeasures.frechet_dist(grouped_df_list[i + num], grouped_df_list[new_index + num])
-    fr_matrix.at[grouped_id_list[i + num], grouped_id_list[new_index + num]] = sim
-    fr_matrix.at[grouped_id_list[new_index + num], grouped_id_list[i + num]] = sim
-
-def fr_distance_matrix(tracker_df):
-    tracker_df["xy_list"] = tracker_df[["smooth_x", "smooth_y"]].values.tolist()
-
-    grouped_df = tracker_df.groupby("UniqueID")
-    grouped_df_list = []
-    grouped_id_list = []
-    len_grouped_df = len(grouped_df)
-    count = 0
-    process_list = []
-
-    for name, group in grouped_df:
-        sys.stdout.write("\r" + f"Constructing lists {round(((count)/(len_grouped_df))*100, 2)}%")
-        sys.stdout.flush()
-        grouped_id_list.append(name)
-        grouped_df_list.append(group["xy_list"].tolist())
-        count += 1
-    names = [x for x in grouped_id_list]
-    fr_matrix = pd.DataFrame(columns=names, index=names)
-    count = 0
-
-    for i in range(len_grouped_df):
-        sys.stdout.write("\r" + f"FR Distance Matrix progress {round(((count)/(len_grouped_df))*100, 2)}%")
-        sys.stdout.flush()
-        for idx in range(0, len(grouped_df_list[i:]), 4):
-            new_index = i + idx
-            try:
-                t1 = Process(target=calculate_assign, args=(i, new_index, grouped_df_list, grouped_id_list, 0))
-                t1.start()
-                process_list.append(t1)
-                t2 = Process(target=calculate_assign, args=(i, new_index, grouped_df_list, grouped_id_list, 1))
-                t2.start()
-                process_list.append(t2)
-                t3 = Process(target=calculate_assign, args=(i, new_index, grouped_df_list, grouped_id_list, 2))
-                t3.start()
-                process_list.append(t3)
-                # sim = similaritymeasures.frechet_dist(grouped_df_list[i], grouped_df_list[new_index])
-                # fr_matrix.at[grouped_id_list[i], grouped_id_list[new_index]] = sim
-                # fr_matrix.at[grouped_id_list[new_index], grouped_id_list[i]] = sim
-
-                # sim = similaritymeasures.frechet_dist(grouped_df_list[i + 1], grouped_df_list[new_index + 1])
-                # fr_matrix.at[grouped_id_list[i + 1], grouped_id_list[new_index + 1]] = sim
-                # fr_matrix.at[grouped_id_list[new_index + 1], grouped_id_list[i + 1]] = sim
-
-                # sim = similaritymeasures.frechet_dist(grouped_df_list[i + 2], grouped_df_list[new_index + 2])
-                # fr_matrix.at[grouped_id_list[i + 2], grouped_id_list[new_index + 2]] = sim
-                # fr_matrix.at[grouped_id_list[new_index + 2], grouped_id_list[i + 2]] = sim
-                try:
-                    t4 = Process(target=calculate_assign, args=(i, new_index, grouped_df_list, grouped_id_list, 3))
-                    t4.start()
-                    process_list.append(t4)
-                    # sim = similaritymeasures.frechet_dist(grouped_df_list[i + 3], grouped_df_list[new_index + 3])
-                    # fr_matrix.at[grouped_id_list[i + 3], grouped_id_list[new_index + 3]] = sim
-                    # fr_matrix.at[grouped_id_list[new_index + 3], grouped_id_list[i + 3]] = sim
-                except KeyError:
-                    pass
-
-                for t in process_list:
-                    t.join()
-
-            except RuntimeError:
-                print ("Error: unable to start thread")
-            
-                if new_index+1 == len_grouped_df:
-                    break
-
-        count += 4
-    # for idx, (_, group) in enumerate(grouped_df):
-    #     count += 1
-    #     group_list = group["xy_list"].tolist()
-    #     sys.stdout.write("\r" + f"FR Distance Matrix progress {round(((count)/(len_grouped_df))*100, 2)}%")
-    #     sys.stdout.flush()
-    #     for idx_2, (_, group_2) in enumerate(grouped_df):
-    #         if idx_2 >= idx:
-    #             group_2_list = group_2["xy_list"].tolist()
-    #             # len_group = len(group_list)
-    #             # len_group_2 = len(group_2_list) 
-    #             # if len_group > len_group_2:
-    #             #     print("G1 " + str(len(group_list)))
-    #             #     print("G2 " + str(len(group_2_list)))
-    #             #     group_2_list += ([] * (len_group - len_group_2))
-    #             #     print("G1_ " + str(len(group_list)))
-    #             #     print("G2_ " + str(len(group_2_list)))
-    #             # elif len_group < len_group_2:
-    #             #     group_list += [0, 0] * (len_group_2 - len_group)
-    #             sim = similaritymeasures.frechet_dist(group_list, group_2_list)
-    #             fr_matrix.at[idx, idx_2] = sim
-    #             fr_matrix.at[idx_2, idx] = sim
-    return tracker_df, fr_matrix
-
-def distance_matrix(vectors):
-    euclidean_d = lambda vector_u, vector_v: max(
-        distance.euclidean(vector_u, vector_v), distance.euclidean(vector_v, vector_u)
-    )
-    count_unique_id = len(vectors)
-    dist_matrix = np.zeros((count_unique_id, count_unique_id))
-
-    count = 0
-    count_unique_id_square = count_unique_id ** 2
-    for i in range(count_unique_id):
-        count += 1
-        # print(f"Distance Matrix progress {round(((count)/(count_unique_id_square))*100, 3)}%", end="\r")
-        # sys.stdout.write("\r" + f"Distance Matrix progress {round(((count)/(count_unique_id_square))*100, 3)}%")
-        # sys.stdout.flush()
-        for j in range(i + 1, count_unique_id, 4):
-            distance_ = euclidean_d(vectors[i], vectors[j])
-            dist_matrix[i, j] = distance_
-            dist_matrix[j, i] = distance_
-    return dist_matrix
+    df_ids = pd.DataFrame(data=df_data, columns=["x", "y", "UniqueID", "frameId", "color", "h", "w"])
+    return df_ids
 
 if __name__ == "__main__":
-
-    df, matrix = fr_distance_matrix(tracker_df_1)
-    raw_combined.tracker_df
-    raw_combined.tracker_df = morph.smooth_tracks(raw_combined.tracker_df, 50)
-    tracker_df_ = run_connect(raw_combined.tracker_df)
-
-    raw_combined.tracker_df
-    tracker_df_1 = morph.cut_tracks_with_few_points(raw_combined.tracker_df, 1000)
-    tracker_df_1
-    # x = itterate_through_df_in_chunks_and_apply(joined.tracker_df)
-    # x
-    tracker_img = morph.get_cv2_point_plot(tracker_df_, (f"{raw_combined.base_image}/{raw_combined.birds_eye_view_image}"), colours = 1) #, joined.labels, joined.uniqueid)
-    morph.show_data(tracker_img)
-
-    # joined.tracker_df["colour"] = []
-    # type(joined.tracker_df)
-    # joined.tracker_df.columns
-    # x.sort_values(by=["UniqueID", "frameId"])
-
-    # # remove_far_field_points(g6.tracker_df, [(0, 0), (1000, 800), (800, 700)])
-
-    # plt.hist([1,2],color=uniqueish_color(125))
-    # uniqueish_color(3)
+    pass
--- Code/main.py
+++ Code/main.py
@@ -1,280 +1,71 @@
-import trackerdf as tdf
-import morph
-import ml
+# import importlib
+# importlib.reload(project)
+import tracker
+import project
 import pandas as pd
-import time
-import pickle
-import _thread
-import sys
-from statistics import mean
+import connect
+import camera_class
 
+# To process the video files go to
+# ------------------------------------------
+# https://colab.research.google.com/drive/13NJxj5pvzDIDWQ5H9GO_bYGqg2dEx5Ey?usp=sharing
 
-class Cameras:
-    def __init__(
-        self,
-        who_is_running_this_code,
-        video_folder,
-        file_name,
-        frame_number=1000,
-        smooth_factor=100,
-        cut_df=20,
-        library=0,
-        parent_path=0,
-        parent_path_video=0,
-        tracker_path=0,
-        video_path=0,
-        photo_path=0,
-        base_image=0,
-        birds_eye_view_image=0,
-    ):
-        self.who_is_running_this_code = who_is_running_this_code
-        self.video_folder = video_folder
-        self.file_name = file_name
-        self.frame_number = frame_number
-        self.smooth_factor = smooth_factor
-        self.cut_df = cut_df
+# Create class object
+# ------------------------------------------
+g6 = camera_class.Camera("hogni", 24032021, "short_g6")
 
-        self.library = "Library"
-        self.parent_path = f"/Users/{self.who_is_running_this_code}/{self.library}/Mobile Documents/com~apple~CloudDocs/Bachelor Project/"
-        self.parent_path_video = f"{self.parent_path}Videos/{self.video_folder}/"
-        self.tracker_path = f"{self.parent_path_video}Data/{self.file_name}/tracker_{self.file_name}.json"
-        self.video_path = f"{self.parent_path_video}Processed/{self.file_name}.mp4"
-        self.photo_path = f"{self.parent_path_video}Photos/{self.file_name}"
-        self.base_image = f"{self.parent_path}Base Image"
-        self.birds_eye_view_image = "FullHD_bridge.png"
+# Create df
+# ------------------------------------------
+# ODC
+g6.tracker_df = tracker.create_tracker_df(g6.tracker_path)
+# YOLOv5 - load
+g6.tracker_df = pd.read_pickle("short_g6_yolov5x6.pickle")
 
-    def make_raw_df(self, name):
-        self.tracker_df = tdf.create_tracker_df(self.tracker_path)
-        self.tracker_df = morph.add_camera(self.tracker_df, name)
-        self.tracker_df = morph.cyclist_contact_coordiantes(self.tracker_df)
+# Road contact point and smooth trajectories
+# ------------------------------------------
+g6.tracker_df = project.cyclist_contact_coordiantes(g6.tracker_df)
+g6.tracker_df = project.smooth_tracks(g6.tracker_df, 20)
 
-    def smooth(self):
-        self.tracker_df = morph.smooth_tracks(self.tracker_df, self.smooth_factor)
+# Remove all tracks with less that n points
+# ------------------------------------------
+g6.tracker_df = project.cut_tracks_with_few_points(g6.tracker_df, 50)
 
-    def cut(self):
-        self.tracker_df = morph.cut_tracks_with_few_points(self.tracker_df, self.cut_df)
-        
-    def warp(self):
-        morph.capture_image_from_video(
-            self.video_path, self.base_image, self.file_name, self.frame_number
-        )
-        src_image_points = morph.click_coordinates(
-            f"{self.base_image}/{self.file_name}.jpg"
-        )
-        dst_image_points = morph.click_coordinates(
-            f"{self.base_image}/{self.birds_eye_view_image}"
-        )
-        self.homo, status = morph.find_homography_matrix(
-            src_image_points, dst_image_points
-        )
-        self.warped_img = morph.warped_perspective(
-            (f"{self.base_image}/{self.file_name}.jpg"),
-            (f"{self.base_image}/{self.birds_eye_view_image}"),
-            self.homo,
-        )
-        morph.show_data(self.warped_img)
-        self.tracker_df = morph.transform_points(self.tracker_df, self.homo)
-        self.tracker_df = morph.smooth_tracks(self.tracker_df, self.smooth_factor)
+# Capture frame from video
+# ------------------------------------------
+video_frame = project.get_frame(g6.video_path, 1000)
 
-    def run_clustering(self):
-        (
-            self.n_clusters,
-            self.labels,
-            self.uniqueid,
-            self.model,
-            self.vectors,
-        ) = ml.run_all(self.tracker_df)
+# Get points on src and dst images
+# ------------------------------------------
+src_image = project.click_coordinates(video_frame)
+dst_image = project.click_coordinates(g6.map_path)
 
-    def plot_and_show(self):
-        try:
-            self.tracker_img = morph.get_cv2_point_plot(
-                self.tracker_df,
-                (f"{self.base_image}/{self.birds_eye_view_image}"),
-                self.labels,
-                self.uniqueid,
-            )
-        except AttributeError:
-            self.tracker_img = morph.get_cv2_point_plot(
-                self.tracker_df, (f"{self.base_image}/{self.birds_eye_view_image}")
-            )
-        morph.show_data(self.tracker_img)
+# Get homography matrix
+# ------------------------------------------
+homo, _ = project.find_homography_matrix(src_image, dst_image)
 
-    def save_object(self, name):
-        with open(f"{name}.pickle", "wb") as output:
-            pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
+# Display warped image
+# ------------------------------------------
+warped_img = project.warped_perspective(video_frame, g6.map_path, homo)
+project.show_data(warped_img)
 
-    def load_pickle(self, name):
-        with open(f"{name}.pickle", "rb") as input:
-            return pickle.load(input)
+# Plot tracks
+# ------------------------------------------
+g6.tracker_df = project.transform_points(g6.tracker_df, homo)
 
-if __name__ == "__main__":
+# Connect points with UniqueID
+# ------------------------------------------
+g6.tracker_df = connect.set_unique_id(g6.tracker_df, max_age=30, min_hits=1, iou_threshold=0.15)
 
-    # Reload a module.
-    import importlib
-    importlib.reload(morph)
+# Plot on MAP
+# ------------------------------------------
+plot = project.get_cv2_point_plot(g6.tracker_df, g6.map_path)
+project.show_data(plot)
 
-    g6 = Cameras("hogni", 24032021, "2403_G6_sync")
-    # g6 = g6.load_pickle("g6_smooth_5_all_points")
-    g6.make_raw_df("g6")
-    g6.warp()
-    g6.save_object("g6_raw")
-    # g6.run_clustering()
-    # g6.plot_and_show()
+# Plot on VIDEO (FRAME)
+# ------------------------------------------
+plot = project.get_cv2_point_plot(g6.tracker_df, video_frame)
+project.show_data(plot)
 
-    s7 = Cameras("hogni", 24032021, "2403_S7_sync")
-    # s7 = s7.load_pickle("s7_smooth_5_all_points")
-    s7.make_raw_df("s7")
-    s7.warp()
-    s7.save_object("s7_raw")
-    # s7.run_clustering()
-    # s7.plot_and_show()
-
-    iph12 = Cameras("hogni", 24032021, "2403_edi_sync")
-    # iph12 = iph12.load_pickle("iph12_smooth_5_all_points")
-    iph12.make_raw_df("iph12")
-    iph12.warp()
-    iph12.save_object("iph12_raw")
-    # iph12.run_clustering()
-    # iph12.plot_and_show()
-
-    raw_combined = Cameras("hogni", 24032021, "2403_G6_sync")
-    raw_combined_df = morph.join_df(g6.tracker_df, s7.tracker_df, iph12.tracker_df)
-    raw_combined_df
-    raw_combined = raw_combined.load_pickle("raw_combined")
-    raw_combined.tracker_df = raw_combined_df
-
-    raw_combined_df.to_csv("raw_combined_df")
-
-    # joined.tracker_df = joined_df
-    raw_combined.save_object("raw_combined")
-    # _thread.start_new_thread(joined.run_clustering())
-    # joined.run_clustering()
-    tracker_img = morph.get_cv2_point_plot(joined.tracker_df, (f"{joined.base_image}/{joined.birds_eye_view_image}"), colours = 1) #, joined.labels, joined.uniqueid)
-    morph.show_data(tracker_img)
-    joined.tracker_df.sort_values(by=["frameId"])[100:150]
-    joined.tracker_df
-
-    tracker_img = morph.get_cv2_point_plot(joined.tracker_df[1000:2000], (f"{joined.base_image}/{joined.birds_eye_view_image}")) #, joined.labels, joined.uniqueid)
-    morph.show_data(tracker_img)
-
-    tracker_img = morph.get_cv2_point_plot(joined.tracker_df[4000:5000], (f"{joined.base_image}/{joined.birds_eye_view_image}")) #, joined.labels, joined.uniqueid)
-    morph.show_data(tracker_img)
-
-    tracker_img = morph.get_cv2_point_plot(joined.tracker_df[5000:6000], (f"{joined.base_image}/{joined.birds_eye_view_image}")) #, joined.labels, joined.uniqueid)
-    morph.show_data(tracker_img)
-
-    tracker_img = morph.get_cv2_point_plot(joined.tracker_df[6000:7000], (f"{joined.base_image}/{joined.birds_eye_view_image}")) #, joined.labels, joined.uniqueid)
-    morph.show_data(tracker_img)
-
-    joined.tracker_df[1000:2000]
-
-    tracker_img = morph.get_cv2_point_plot(joined.tracker_df, (f"{joined.base_image}/{joined.birds_eye_view_image}")) #, joined.labels, joined.uniqueid)
-    morph.show_data(tracker_img)
-
-    # count = 0
-    # for i in range(max(g6.tracker_df["frameId"]):
-    #     g6_track = g6.tracker_df[g6.tracker_df["frameId"] == i]
-    #     uni = morph.get_cv2_point_plot(g6_track, (f"{g6.base_image}/{g6.birds_eye_view_image}"))
-    #     morph.show_data(uni)
-
-    # count = 0
-    # for name, group in s7.tracker_df.groupby("UniqueID"):
-    #     s7_track = s7.tracker_df[s7.tracker_df["UniqueID"] == name]
-    #     uni_s7 = morph.get_cv2_point_plot(s7_track, (f"{s7.base_image}/{s7.birds_eye_view_image}"))
-    #     morph.show_data(uni_s7)
-    #     count += 1
-    #     if count == 5:
-    #         break
-
-    # outer = pd.concat([g6.tracker_df, s7.tracker_df])
-    # outer = pd.concat([outer, iph12.tracker_df])
-
-    # outer = outer.sort_values(by=["frameId"])
-    # outer = Cameras("hogni", 24032021, "2403_edi_sync")
-    # x.tracker_df = outer
-    # x.save_object("concatenated")
-    # outer = outer.load_pickle("concatenated")
-    # outer_grouped = outer.tracker_df.groupby("UniqueID")
-    # outer.tracker_df[1000:1100]
-    # frame_id = set()
-    # un_id = []
-    # for count, (name, row) in enumerate(outer.iterrows()):
-    #     un_id.append(row["UniqueID"])
-    #     frame_id.add(row["frameId"])
-    #     if len(frame_id) == 100:
-    #         df = outer[outer["UniqueID"].isin(un_id)]
-    #         mor = morph.get_cv2_point_plot(df, (f"{s7.base_image}/{s7.birds_eye_view_image}"))
-    #         morph.show_data(mor)
-    #         time.sleep(1)
-    #         frame_id = set()
-    #     if count == 20000:
-    #         break
-
-    # import cv2
-
-    # def video(outer):
-    #     # out = cv2.VideoWriter("output.avi", -1, 10.0, (1920, 1080))
-
-    #     previous_i = 1000
-    #     len_df = len(outer)
-    #     for i in range(0, len_df, 1000):
-    #         # sys.stdout.write("\r" + f"Video Progress {round(((i)/(len_df))*100, 2)}%")
-    #         # sys.stdout.flush()
-    #         mor = morph.get_cv2_point_plot(
-    #             outer[i:previous_i], (f"{s7.base_image}/{s7.birds_eye_view_image}")
-    #         )
-    #         morph.show_data(mor)
-    #         # out.write(mor)
-    #         previous_i += i
-
-    #     # out.release()
-    #     # cv2.destroyAllWindows()
-
-    # video(outer)
-
-    ########
-    # Trying to join
-    # outer_grouped = outer.tracker_df.groupby("UniqueID")
-    # previous_i = 30
-    # len_df = len(outer.tracker_df)
-    # len_df**3
-
-    # def are_overlapping(point_one, point_two):
-    #     return not(point_one[1] < point_two[0] or point_two[1] < point_one[0])
-
-    # for i in range(0, len_df, 30):
-    #     unique_id = outer.tracker_df[i:previous_i].UniqueID.unique()
-    #     for id in unique_id:
-    #         track_one = outer.tracker_df[outer.tracker_df["UniqueID"] == id]
-
-    #         for a in range(1, len(unique_id)):
-    #             track_two = outer.tracker_df[outer.tracker_df["UniqueID"] == unique_id[a]]
-
-    #             # Check bearings aboutt eh same
-    #             abs_1 = mean(track_one["bearing"])
-    #             abs_2 = mean(track_two["bearing"])
-    #             if abs_1 > abs_2:
-    #                 check = abs_1 - abs_2
-    #             else:
-    #                 check = abs_2 - abs_1
-    #             if check <= 90:
-    #                 over = []
-    #                 # print(track_one)
-    #                 for _, b in track_one.iterrows():
-    #                     for _, c in track_two.iterrows():
-    #                         overlapping = are_overlapping((b["x"],b["y"]), (c["x"],c["y"]))
-    #                         if overlapping:
-    #                             over.append(1)
-    #                 if len(over) >= 10:
-    #                     outer.tracker_df["UniqueID"].loc[(outer.tracker_df["UniqueID"] == unique_id[a])] = id
-    #                 over = []
-    #     previous_i += i
-
-    # outer = Cameras("hogni", 24032021, "2403_edi_sync")
-    # outer = outer.load_pickle("concatenated")
-    # outer.tracker_df
-
-    # new_df =pd.DataFrame(columns=["UniqueID", "frameId", "bearing", "altered_x", "altered_y", "camera"])
-    # for name, group in outer.tracker_df.groupby("frameId"):
-    #     for _, i in group.iterrows():
-    #         pass
+# df to CSV
+# ------------------------------------------
+g6.tracker_df.to_csv("short_g6_yolov5x6_id.csv")
