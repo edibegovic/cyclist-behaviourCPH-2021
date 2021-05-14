4bc26983c9a1821dc9f61f71917eb4e480f200c1
Høgni <hogniziska@gmail.com>
2021-05-14 17:11:41 +0200

    .

--- Code/main.py
+++ Code/main.py
@@ -22,8 +22,8 @@ if __name__ == "__main__":
     # g6.tracker_df = color_id(g6.tracker_df)
     # g6.color_label()
     # g6.add_color("label")
-    plot = g6.plot_object(g6.tracker_df, g6.map_path)
-    g6.show_data("Warped img", plot)
+    # plot = g6.plot_object(g6.tracker_df, g6.map_path)
+    # g6.show_data("Warped img", plot)
 
     s7 = cyclist.Camera("2403_s7_sync", "s7")
     s7.read_pkl("2403_S7_sync_yolov5x6_corrected")
@@ -41,18 +41,18 @@ if __name__ == "__main__":
     # Plot image.
     # s7.color_label()
     # s7.add_color("label")
-    plot = s7.plot_object(s7.tracker_df, s7.map_path)
-    s7.show_data("Warped img", plot)
+    # plot = s7.plot_object(s7.tracker_df, s7.map_path)
+    # s7.show_data("Warped img", plot)
 
     remove_line = g6.click_coordinates(g6.map_path, dst = 0, type = "line")
     g6.remove_point_line(remove_line, "below")
     s7.remove_point_line(remove_line, "above")
 
-    joined = cyclist.Camera("joined", "joined")
+    joined = cyclist.Camera("joined_corrected", "joined")
     joined.tracker_df = join_df([g6.tracker_df, s7.tracker_df])
     joined.new_bbox(5)
     joined.df_format()
-    joined.unique_id(max_age=30, min_hits=1, iou_threshold=0.15, save_load = "new")
+    joined.unique_id(max_age=90, min_hits=1, iou_threshold=0.15, save_load = "load")
     # joined.cut_tracks_with_few_points(50)
 
     # joined.add_bearing()
@@ -61,8 +61,8 @@ if __name__ == "__main__":
     # joined.add_color(type="rainbow")
 
     # joined.color_label()
-    # joined.add_color("label")
+    # joined.add_color()
     plot = joined.plot_object(joined.tracker_df, joined.map_path)
     joined.show_data("Warped img", plot)
 
-    # joined.tracker_df.to_csv("CSV/joined_df_corrected_90_1_0.15_bbox10.csv")
+    joined.tracker_df.to_csv("CSV/joined_df_corrected_90_1_0.15_bbox5_delete.csv")
